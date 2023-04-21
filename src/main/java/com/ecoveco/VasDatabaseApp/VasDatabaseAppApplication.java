d7c5dece5073b4db18df4814651f1cf5b2736080
vdionysio <vini.dionysio@gmail.com>
2023-04-21 13:03:20 -0300

    core: create backend spring boot project

---
+++ src/main/java/com/ecoveco/VasDatabaseApp/VasDatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecoveco.VasDatabaseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasDatabaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasDatabaseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoveco/VasDatabaseApp/VasDatabaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoveco.VasDatabaseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasDatabaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
